96f29a0ab5032911af2fa831d8c1db2531d358c8
Fatıma Saliha Biter <f.saliha.biter@hotmail.com>
2021-04-05 17:33:49 +0300

    05.04.2021 10. Gün Sonu

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Ürün eklendi.";
+        public static string ProductNameInvalid = "Ürün ismi geçersizdir.";
+        internal static string MaintenanceTime = "Sistem bakımda";
+        internal static string PrductListed = "Ürünler listelendi.";
+    }
+}
--- Test/Program.cs
+++ Test/Program.cs
@@ -30,11 +30,20 @@ namespace ConsoleUI
         {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetProductDetails())
+            var result = productManager.GetProductDetails();
+
+            if (result.Success == true)
             {
+                foreach (var product in result.Data)
+                {
 
-                Console.WriteLine(product.ProductName + " - " + product.CategoryName);
+                    Console.WriteLine(product.ProductName + " - " + product.CategoryName);
 
+                }
+            }
+            else
+            {
+                Console.WriteLine(result.Message);
             }
         }
     }
